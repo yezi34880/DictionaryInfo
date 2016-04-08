16b56a44d16a318f10c80d1eba30ca9c5b33a436
YY <yuyang34880@163.com>
2016-04-08 19:28:35 +0800

    把文件夹伪装成系统程序（比如回收站等等），关键是双击就真的打开回收站了，藏羞羞的东西再也不用担心会被发现了，     但是好像有些延迟，偶尔会报错，以前可以，现在换了win10好像有点问题，不想改了。

---
+++ DictionaryInfo/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace DictionaryInfo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowser = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.cboType = new System.Windows.Forms.ComboBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBrowser
+            // 
+            this.btnBrowser.Location = new System.Drawing.Point(209, 39);
+            this.btnBrowser.Name = "btnBrowser";
+            this.btnBrowser.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowser.TabIndex = 0;
+            this.btnBrowser.Text = "浏览";
+            this.btnBrowser.UseVisualStyleBackColor = true;
+            this.btnBrowser.Click += new System.EventHandler(this.btnBrowser_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(12, 12);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(272, 21);
+            this.txtPath.TabIndex = 1;
+            // 
+            // cboType
+            // 
+            this.cboType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboType.FormattingEnabled = true;
+            this.cboType.Items.AddRange(new object[] {
+            "我的电脑",
+            "我的文档",
+            "控制面板",
+            "记事本",
+            "网络邻居 ",
+            "回收站 ",
+            "Internet Explorer",
+            "Windows Media Player"});
+            this.cboType.Location = new System.Drawing.Point(12, 97);
+            this.cboType.Name = "cboType";
+            this.cboType.Size = new System.Drawing.Size(272, 20);
+            this.cboType.TabIndex = 2;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(73, 319);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 3;
+            this.btnOK.Text = "伪装";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(171, 319);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "还原";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 354);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.cboType);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnBrowser);
+            this.Name = "Form1";
+            this.Text = "伪装文件夹";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowser;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ComboBox cboType;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ DictionaryInfo/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DictionaryInfo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            cboType.SelectedIndex = 0;
+        }
+
+        private void btnBrowser_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            txtPath.Text = folderBrowserDialog1.SelectedPath.ToString();
+        }
+
+        private string getType(string type)
+        {
+            switch (type)
+            {
+                case "我的电脑":
+                    return @"{20D04FE0-3AEA-1069-A2D8-08002B30309D}";
+                case "我的文档":
+                    return @"{450D8FBA-AD25-11D0-98A8-0800361B1103}";
+                case "控制面板":
+                    return @"{21EC2020-3AEA-1069-A2DD-08002B30309D}";
+                case "网络邻居":
+                    return @"{208D2C60-3AEA-1069-A2D7-08002B30309D}";
+                case "回收站":
+                    return @"{645FF040-5081-101B-9F08-00AA002F954E}";
+                default:
+                    return @"{20D04FE0-3AEA-1069-A2D8-08002B30309D}";
+            }
+        }
+
+        private void CamouFlage(string str)
+        {
+            try
+            {
+                StreamWriter sw = File.CreateText(txtPath.Text + @"\desktop.ini");
+                sw.WriteLine(@"[.ShellClassInfo]");
+                sw.WriteLine("CLSID=" + str);
+                sw.Close();
+
+                //设置desktop.ini为隐藏
+                File.SetAttributes(txtPath.Text + @"\desktop.ini", FileAttributes.Hidden | FileAttributes.ReadOnly | FileAttributes.System | FileAttributes.Archive);
+                File.SetAttributes(txtPath.Text, FileAttributes.System | FileAttributes.ReadOnly);
+                MessageBox.Show("伪装成功！", "提示");
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+
+
+        }
+
+        private void Restore()
+        {
+            try
+            {
+                FileInfo fi = new FileInfo(txtPath.Text + @"\desktop.ini");
+                if (fi.Exists==false)
+                {
+                    MessageBox.Show("该文件夹没有被伪装！","提示");
+                    return;
+                }
+                else
+                {
+                    System.Threading.Thread.Sleep(500);
+                    File.SetAttributes(txtPath.Text + @"\desktop.ini", FileAttributes.Hidden);
+                    fi.Delete();
+                    File.SetAttributes(txtPath.Text, FileAttributes.System);
+                    MessageBox.Show("还原成功！", "提示");
+                }
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (txtPath.Text.Trim() == "")
+                return;
+            if (cboType.Text!="")
+            {
+                string str = getType(cboType.Text);
+                CamouFlage(str);
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            if (txtPath.Text.Trim() == "")
+                return;
+            Restore();
+
+        }
+
+
+
+    }
+}
